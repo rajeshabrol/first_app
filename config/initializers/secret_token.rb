01037966bc8e375eb3fe2c8071317b05e758a33d
rajesha1 <root@ubuntu.(none)>
2011-05-25 12:04:22 -0700

    Install comitt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '580b954c4d32bf3aa0a112d8c58959db1edf84a2579609a5cb477eb409c4cf8d0c2967273173360ac0a505f283bfa04878ec3609e49a8d3c6e18acd6d4d3c1e9'
